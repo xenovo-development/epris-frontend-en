426906bbf3b6a987798a21477ee7828c0ebd2bc3
damlahocaoglu <damlahocaogluu@gmail.com>
2024-09-19 12:51:16 +0300

    update angle right

--- components/BreadcrumbOne.vue
+++ components/BreadcrumbOne.vue
@@ -1,27 +1,42 @@
 <template>
-    <div class="page-title-section section-padding-top bg-cover" data-overlay="0.7" :style="{ backgroundImage: `url(${backgroundUrl})` }">
-        <div class="page-title">
-            <div class="container">
-                <h1 class="title">{{ title }}</h1>
-            </div>
-        </div>
-        <div class="page-breadcrumb position-static">
-            <div class="container">
-                <ul class="breadcrumb page-breadcrumb">
-                    <li class="breadcrumb-item">
-                        <nuxt-link to="/">Startseite</nuxt-link>
-                    </li>
-                    <li class="breadcrumb-item">
-                        <span>{{ activePageName }}</span>
-                    </li>
-                </ul>
-            </div>
-        </div>
+  <div
+    class="page-title-section section-padding-top bg-cover"
+    data-overlay="0.7"
+    :style="{ backgroundImage: `url(${backgroundUrl})` }"
+  >
+    <div class="page-title">
+      <div class="container">
+        <h1 class="title">{{ title }}</h1>
+      </div>
     </div>
+    <div class="page-breadcrumb position-static">
+      <div class="container">
+        <ul class="breadcrumb page-breadcrumb">
+          <li class="breadcrumb-item">
+            <nuxt-link to="/">Startseite</nuxt-link>
+          </li>
+          <i class="fa fa-angle-right"></i>
+          <li class="breadcrumb-item">
+            <span>{{ activePageName }}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        props: ['activePageName', 'title', 'backgroundUrl']
-    };
+export default {
+  props: ["activePageName", "title", "backgroundUrl"],
+};
 </script>
+
+<style>
+.fa-angle-right {
+  margin: 0 15px;
+  font-size: 16px;
+  color: #fff3df !important;
+  margin-top: 4px;
+  padding-right: 0 !important;
+}
+</style>
